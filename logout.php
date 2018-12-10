c1469344cc1f869e3fbe364d4580048f923f5006
Gary <grr4558@uncw.edu>
2018-12-09 23:43:55 -0500

    Fixed problems with logout.php page

--- logout.php
+++ logout.php
@@ -1,27 +1,24 @@
 <?php
-session_start();
-require_once('../mysqli_config2.php');
-include 'includes/header.php';
-<<<<<<< HEAD
-if (!isset($_SESSION['email'])){
-=======
-if (!isset($_SESSION['EmailAddress'])){
->>>>>>> origin/master
-	$message1 = 'Sorry';
-	$message2 = 'You have reached this page in error!';
-}
-else
-{
-	$_SESSION=[];
-	session_destroy();
-	setcookie('PHPSESSID', '', time()-3600, '/', '', 0, 0);
-	$message1 = 'You have logged out successfully';
-	$message2 = 'THANK YOU FOR BEING PART OF US!';
-}
+	session_start();
+	require_once('../mysqli_config2.php');
+	include 'includes/header.php';
+	if (!isset($_SESSION['EmailAddress']))
+	{
+		$message1 = 'Sorry';
+		$message2 = 'You have reached this page in error!';
+	}
+	else
+	{
+		$_SESSION=[];
+		session_destroy();
+		setcookie('PHPSESSID', '', time()-3600, '/', '', 0, 0);
+		$message1 = 'You have logged out successfully';
+		$message2 = 'THANK YOU FOR BEING PART OF US!';
+	}
 
-echo "<main>";
-echo "<h1>$message1</h1>";
-echo "<h2>$message2</h2>";
-echo "</main>";
-include 'includes/footer.php';
+	echo "<main>";
+	echo "<h1>$message1</h1>";
+	echo "<h2>$message2</h2>";
+	echo "</main>";
+	include 'includes/footer.php';
 ?>
