4b5c232e27c99b76a09815525e01f21243f934a8
Gary <grr4558@uncw.edu>
2018-12-10 21:41:03 -0500

    Edited the index.php file

--- index.php
+++ index.php
@@ -8,12 +8,6 @@
 		<p id="main_p">
 			Coming soon to Jacksonville, NC. Marichuy's Taqueria, a real street tacos restaurant where you will enjoy the best of authentic Mexican food. Please keep visiting us for more updates, or if you would like to start by being part of this amazing journey feel free to sign up to create your account and set your profile to be ready for making online orders or just be updated on more information about our food or specials.
 		</p>
-		
-		<!--<img src="images/IMG-20181015-WA0000.jpg"><br>
-		<img src="images/IMG-20181015-WA0001.jpg"><br>
-		<img src="images/IMG-20181015-WA0003.jpg"><br>
-		<img src="images/IMG-20181015-WA0004.jpg"><br>
-		<img src="images/IMG-20181015-WA0004.jpg"><br>-->
 	</main>
 <?php 
 	include './includes/footer.php';
